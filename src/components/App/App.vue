fe4e2f569588e7c5209b1487364faaf17ca9a027
marcomuraro4 <marco1.muraro@mail.polimi.it>
2023-07-22 09:26:55 +0200

    Project commit

---
+++ src/components/App/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <SynthDevice></SynthDevice>
+</template>
+
+<script src="./App.js"></script>
+
+<style src="./App.css"></style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import * as Vue from 'vue'
+import App from './components/App/App.vue'
+import * as Tone from 'tone'
+
+const app = Vue.createApp(App)
+app.config.globalProperties.$tone = Tone
+
+app.mount('#app')
+
+// Remember to import fonts in index.html
